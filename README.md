982039d2fafd54abb08e81a0a24b8ee5618ab794
Richard Boardman <rickschmoo@gmail.com>
2018-09-22 12:49:29 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Minimal Create React App / Node Express API
+
+Server
+- if you don't have nodemon installed globally: `npm i nodemon -g`
+- `npm install`
+- test: `npm run server` and load `http://localhost:5000/api/hello`
+
+Client
+- make sure server by following above steps
+- `cd client/`
+- `npm install`
+- `cd ..` 
+- test: `npm run client` and load `http://localhost:3000/`
+
+Thankyou
+- https://medium.freecodecamp.org/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0
---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState(
+        { response: res.express }
+      ))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="api-test">
+          {this.state.response}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
